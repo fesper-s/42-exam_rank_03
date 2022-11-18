da1c232cf03533db4664973e1c97e88038267f81
fesper-s <fafaeesfabricio@gmail.com>
2022-11-18 07:39:38 -0300

    Updated shorter gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -2,13 +2,7 @@
 
 char	*get_next_line(int fd)
 {
-	char	*str = malloc(10000);
-	char	*buffer = str;
-
-	while (read(fd, buffer, 1) > 0 && *buffer != '\n')
-		buffer++;
-	if (buffer > str)
-		return (*buffer = 0, str);
-	else
-		return (free(str), NULL);
+	char	*str = malloc(10000), *buffer = str;
+	while (read(fd, buffer, 1) > 0 && *buffer++ != '\n');
+	return (buffer > str) ? (*buffer = 0, str) : (free(str), NULL);
 }
