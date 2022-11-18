a088c003756724b077ef62d19a33f5f6ad7b6dc6
fesper-s <fafaeesfabricio@gmail.com>
2022-11-18 08:33:26 -0300

    Updated shorter printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,77 +1,41 @@
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
 
-int	g_var = 0;
-
-void	ft_putchar(char c)
+size_t	ft_putstr(char *str, int len)
 {
-	write(1, &c, 1);
+	while (str && str[len] && ++len);
+	return (str ? write(1, str, len) : write(1, "(null)", 6));
 }
 
-void	ft_putnbr(int n)
-{
-	long	nbr;
-
-	nbr = (long) n;
-	if (nbr < 0)
-	{
-		ft_putchar('-');
-		nbr *= -1;
-	}
-	if (nbr >= 10)
-	{
-		ft_putnbr(nbr / 10);
-		nbr = nbr % 10;
-	}
-	ft_putchar(nbr + 48);
-}
-
-void	ft_puthex(unsigned nbr, unsigned len, char *sign)
+void	ft_putnbr(long nbr, unsigned len, char *sign, int *size)
 {
 	if (nbr >= len)
-		ft_puthex(nbr / len, len, sign);
-	write(1, &sign[nbr % len], 1);
-	g_var++;
+		ft_putnbr(nbr / len, len, sign, size);
+	*size += (int) write(1, &sign[nbr % len], 1);
+}
+
+void	ft_puthex(long nbr, int len, char *sign, int *size)
+{
+	(nbr < 0) ? (*size += (int) write(1, "-", 1), \
+		ft_putnbr(-nbr, len, sign, size)) : (ft_putnbr(nbr, len, sign, size));
 }
 
 int	ft_printf(char *format, ...)
 {
-	g_var = 0;
+	int		size = 0;
 	va_list	ap;
 
 	va_start(ap, format);
 	while (*format)
 	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 's')
-			{
-				int len = -1;
-				char *str = va_arg(ap, char *);
-				if (!str)
-					str = "(null)";
-				while (str[++len]);
-				g_var += len;
-				write(1, str, len);
-			}
-			else if (*format == 'd')
-			{
-				long long decimal = va_arg(ap, int);
-				if (decimal < 0)
-					g_var++;
-				ft_putnbr(decimal);
-			}
-			else if (*format == 'x')
-			{
-				int hex = va_arg(ap, int);
-				ft_puthex(hex, 16, "0123456789abcdef");
-			}
-			format++;
-		}
+		if (*format == '%' && *(format + 1) == 's' && (format += 2))
+			size += (int) ft_putstr(va_arg(ap, char *), 0);
+		else if (*format == '%' && *(format + 1) == 'x' && (format += 2))
+			ft_putnbr(va_arg(ap, int), 16, "0123456789abcdef", &size);
+		else if (*format == '%' && *(format + 1) == 'd' && (format += 2))
+			ft_puthex(va_arg(ap, int), 10, "0123456789", &size);
 		else
-			g_var += write(1, format++, 1);
+			size += (int) write(1, format++, 1);
 	}
-	va_end(ap);
-	return (g_var);
+	return (va_end(ap), size);
 }
